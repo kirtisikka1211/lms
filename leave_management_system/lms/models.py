c6efcc1a18f8d56de6bdfb6420dfa976c44454b0
kirtisikka <kirtisikka972@gmail.com>
2023-03-15 16:12:13 +0530

    data of memebers

--- leave_management_system/lms/models.py
+++ leave_management_system/lms/models.py
@@ -1,13 +1,17 @@
 from django.db import models
-
 class Members(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email = models.EmailField()
+    gender=models.CharField(max_length=50)
     status = models.CharField(max_length=100)
+    email = models.EmailField()
+    # def __str__(self):
+    #     return self.first_name
+    
+def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice_text = models.CharField(max_length=200)
+#     votes = models.IntegerField(default=0)
