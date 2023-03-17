2122b88d6a4953b39cad05b6035fc943e36feaa5
KshitijThareja <kshitijthareja03@gmail.com>
2023-03-17 10:15:25 +0530

    Add filters for dashboard

--- leave_management_system/lms/models.py
+++ leave_management_system/lms/models.py
@@ -5,14 +5,6 @@ class Members(models.Model):
     status = models.CharField(max_length=100)
     email = models.EmailField()
     year = models.IntegerField()
-    # def __str__(self):
-    #     return self.first_name
+
     def __str__(self):
         return f"{self.first_name}"
-    
-
-
-# class Choice(models.Model):
-#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#     choice_text = models.CharField(max_length=200)
-#     votes = models.IntegerField(default=0)
--- leave_management_system/lms/views.py
+++ leave_management_system/lms/views.py
@@ -1,11 +1,31 @@
 from django.http import HttpResponse
 from .models import Members
 from django.shortcuts import render
-
+# from .filters import OrderFilter
+from django.db.models import Q
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the polls index.")
 def members_list(request):
     members = Members.objects.all()
+    # members = Members.objects.get(id=pk)
+    # name=members.first_name.all()
+    # mail= members.email.all()
+    # gen=members.gender.all()
+    # stat= members.status.all()
+    # years=members.year.all()
+    # myFilter= OrderFilter(request.GET, queryset= name)
+    # name= myFilter.qs
     return render(request, 'dashboard/members_list.html', {'members': members})
-# views.py
+# Define function to search book
+# def search(request):
+#     results = []
 
+#     if request.method == "GET":
+#         query = request.GET.get('search')
+
+#         if query == '':
+#             query = 'None'
+
+#         results = Members.objects.filter(Q(first_name__icontains=query) | Q(gender__icontains=query) | Q(email__icontains=query) | Q(status__icontains=query) | Q(year__icontains=query))
+
+#     return render(request, 'members_list.html', {'query': query, 'results': results})
