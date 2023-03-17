b5fec0abc5446937da6dfd922e42326356eb5c2f
kirtisikka <kirtisikka972@gmail.com>
2023-03-17 10:41:51 +0530

    worked filter options

--- leave_management_system/lms/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Members
-admin.site.register(Members)
-# Register your models here.
--- leave_management_system/lms/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.members_list, name='members'),
-]
