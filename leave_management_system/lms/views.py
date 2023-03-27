ceed001bd5756a08d04db3e81b13ec78148fd35e
KshitijThareja <kshitijthareja03@gmail.com>
2023-03-27 13:32:10 +0530

    Correct html form

--- leave_management_system/lms/views.py
+++ leave_management_system/lms/views.py
@@ -11,7 +11,16 @@ from django.contrib import messages
 #     return HttpResponse("Hello, world. You're at the polls index.")
 def members_list(request):
     members = Members.objects.all()
-    return render(request, 'dashboard/members_list.html', {'members': members})
+    value= False
+    val= False
+
+    for det in members:
+        uname= det.username
+        if uname==request.user.username:
+            if det.year==2020 or det.year==2021:
+                print(det.year)
+                value= True
+    return render(request, 'dashboard/members_list.html', {'members': members,"value": value, "val": val })
 def my_view(request):
     user = request.user
     context = {'user': user}
@@ -20,42 +29,42 @@ def my_view(request):
 def leave_request(request):
     if request.method == 'POST':
         start_date = request.POST.get('start-date')
+        print(start_date)
         end_date = request.POST.get('end-date')
         reason = request.POST.get('reason')
         mem = Members.objects.all()
         
-        print(request.user.username)
         for member in mem:
             username = member.username 
-            print(username)
             if username == request.user.username:
-                # mentor = member.mentor
                 email = member.mentoremail
-                print(email)
+                
         maillist= email.split(", ")
         print(maillist)
         msg = EmailMultiAlternatives('Leave request', f'Start Date: {start_date}\nEnd Date: {end_date}\nReason: {reason}', EMAIL_HOST_USER, maillist)
-                
-            
-        if msg.send():
-            messages.success(request, 'Leave request submitted successfully.')      
-        else:
-            messages.error(request, 'Leave request unsuccessful.')
+        try:
 
-    memb=Members.objects.all() 
-    value= False
-    val= False
-
-    for det in memb:
-        uname= det.username
-        if uname==request.user.username:
-            if det.year==2020 or det.year==2021:
-                print(det.year)
-                value= True
-                
-    dict= {"value": value, "val": val}
-
-    return render(request, 'dashboard/leave_request.html', dict)   
+            start_list= start_date.split("-")
+            end_list= end_date.split("-")
+            print(start_list)
+            if int(start_list[1])==int(end_list[1]):
+                if int(start_list[2])< int(end_list[2]):
+                    msg.send()
+                    messages.success(request, 'Leave request submitted successfully.')  
+                else:  
+                    messages.error(request, 'Please mention a valid time period.')
+            elif int(start_list[1])< int(end_list[1]):
+                msg.send()
+                messages.success(request, 'Leave request submitted successfully.')
+            elif int(start_list[0])< int(end_list[0]):
+                msg.send()
+                messages.success(request, 'Leave request submitted successfully.')
+             
+            else:
+                messages.error(request, 'Leave request unsuccessful, please give valid details.')
+        except:
+            messages.error(request, 'Leave request unsuccessful, please give valid details.')
+    return render(request, 'dashboard/leave_request.html')   
 def user(request, id):
     user = get_object_or_404(Members, id=id) 
     members = Members.objects.all()
